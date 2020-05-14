27d39a5a8419fbeacf381a453bed1b77902b7238
kaizu <kwaizu@gmail.com>
2020-05-14 23:18:11 +0900

    Support orca

--- misc/main.sh
+++ misc/main.sh
@@ -7,31 +7,35 @@ ls .
 export ARTIFACTS_PATH=/artifacts
 cd en
 
-papermill top.ipynb ${ARTIFACTS_PATH}/top.ipynb
-
-mkdir -p ${ARTIFACTS_PATH}/tutorials
-cd tutorials
-for filename in *.ipynb; do
-    echo ${filename}
-    name=`basename "$filename"`
-    papermill ${name} ${ARTIFACTS_PATH}/tutorials/${name}
-done
-cd ..
-
-mkdir -p ${ARTIFACTS_PATH}/tests
-cd tests
-for filename in *.ipynb; do
-    echo ${filename}
-    name=`basename "$filename"`
-    papermill ${name} ${ARTIFACTS_PATH}/tests/${name}
-done
-cd ..
+# papermill top.ipynb ${ARTIFACTS_PATH}/top.ipynb
+# 
+# mkdir -p ${ARTIFACTS_PATH}/tutorials
+# cd tutorials
+# for filename in *.ipynb; do
+#     echo ${filename}
+#     name=`basename "$filename"`
+#     papermill ${name} ${ARTIFACTS_PATH}/tutorials/${name}
+# done
+# cd ..
+# 
+# mkdir -p ${ARTIFACTS_PATH}/tests
+# cd tests
+# for filename in *.ipynb; do
+#     echo ${filename}
+#     name=`basename "$filename"`
+#     papermill ${name} ${ARTIFACTS_PATH}/tests/${name}
+# done
+# cd ..
+# 
+# mkdir -p ${ARTIFACTS_PATH}/examples
+# cd examples
+# for filename in *.ipynb; do
+#     echo ${filename}
+#     name=`basename "$filename"`
+#     papermill ${name} ${ARTIFACTS_PATH}/examples/${name}
+# done
+# cd ..
 
 mkdir -p ${ARTIFACTS_PATH}/examples
 cd examples
-for filename in *.ipynb; do
-    echo ${filename}
-    name=`basename "$filename"`
-    papermill ${name} ${ARTIFACTS_PATH}/examples/${name}
-done
-cd ..
+papermill example05.ipynb ${ARTIFACTS_PATH}/examples/example05.ipynb
--- misc/run.sh
+++ misc/run.sh
@@ -2,4 +2,4 @@
 
 ROOTPATH=$(pwd)
 docker build -t ecell4_docs ${ROOTPATH}
-docker run --rm -e LOCAL_UID=$(id -u $USER) -e LOCAL_GID=$(id -g $USER) --mount type=bind,src=${ROOTPATH}/artifacts,dst=/artifacts -it ecell4_docs:latest
+docker run --device /dev/fuse --cap-add SYS_ADMIN --rm -e LOCAL_UID=$(id -u $USER) -e LOCAL_GID=$(id -g $USER) --mount type=bind,src=${ROOTPATH}/artifacts,dst=/artifacts -it ecell4_docs:latest
